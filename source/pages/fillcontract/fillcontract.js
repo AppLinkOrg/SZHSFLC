99de299b584d8790b27c5b1e1494c175b7041c31
cxj17 <1609771729@qq.com>
2019-03-15 20:13:49 +0800

    commit

---
+++ source/pages/fillcontract/fillcontract.js
@@ -0,0 +1,52 @@
+
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+
+    this.Base.setMyData({
+      ctt: 1
+    });
+
+  }
+  onMyShow() {
+    var that = this;
+  }
+
+  bindwaitcompleted(e) {
+    this.Base.setMyData({
+      ctt: 2
+    })
+    this.onMyShow();
+  }
+  bindcontact(e) {
+    this.Base.setMyData({
+      ctt: 1
+    })
+    this.onMyShow();
+  }
+
+
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+body.bindcompleted = content.bindcompleted;
+body.bindwaitcompleted = content.bindwaitcompleted;
+body.bindcontact = content.bindcontact;
+Page(body)
---
+++ source/pages/login/login.js
@@ -0,0 +1,45 @@
+// pages/login/login.js
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+  onMyShow() {
+    var that = this;
+  }
+  login() {
+    wx.switchTab({
+      url: '../index/index',
+    })
+  }
+
+  getPhoneNumber(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+  }
+
+}
+
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+body.one = content.one;
+body.login = content.login;
+body.getPhoneNumber = content.getPhoneNumber;
+Page(body)
