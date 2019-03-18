be4f3dbf554c6eff68af5b34ed10923fc6db6d17
cxj17 <1609771729@qq.com>
2019-03-18 19:30:57 +0800

    commit

--- source/pages/mine/mine.js
+++ source/pages/mine/mine.js
@@ -1,7 +1,13 @@
-// pages/mine/mine.js
-import { AppBase } from "../../appbase";
-import { ApiConfig } from "../../apis/apiconfig";
-import { InstApi } from "../../apis/inst.api.js";
+
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
 
 class Content extends AppBase {
   constructor() {
@@ -11,13 +17,36 @@ class Content extends AppBase {
     this.Base.Page = this;
     //options.id=5;
     super.onLoad(options);
+
+    this.Base.setMyData({
+      ctt: 1
+    });
+
   }
   onMyShow() {
     var that = this;
   }
+
+  bindwaitcompleted(e) {
+    this.Base.setMyData({
+      ctt: 2
+    })
+    this.onMyShow();
+  }
+  bindcontact(e) {
+    this.Base.setMyData({
+      ctt: 1
+    })
+    this.onMyShow();
+  }
+
+
 }
 var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
+body.bindcompleted = content.bindcompleted;
+body.bindwaitcompleted = content.bindwaitcompleted;
+body.bindcontact = content.bindcontact;
 Page(body)
